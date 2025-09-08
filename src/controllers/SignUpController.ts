39ff035f3237f2ad645d11d28078db8ededb6339
Julio Cesar <juliocesarnb99@gmail.com>
2025-09-08 17:10:54 -0300

    New tasks

--- src/controllers/SignUpController.ts
+++ src/controllers/SignUpController.ts
@@ -1,50 +1,73 @@
-import { HttpRequest, HttpResponse } from "../types/Http";
-import { badRequest, created } from "../utils/http";
-import { z } from "zod";
-
+import { hash } from 'bcryptjs';
+import { eq } from 'drizzle-orm';
+import z from 'zod';
+import { db } from '../db';
+import { usersTable } from '../db/schema';
+import { calculateGoals } from '../lib/calculateGoals';
+import { signAccessTokenFor } from '../lib/jwt';
+import { HttpRequest, HttpResponse } from '../types/Http';
+import { badRequest, conflict, created } from '../utils/http';
 
 const schema = z.object({
-    goal: z.enum(['lose', 'maintain', 'gain']),
-    gender: z.enum(['male', 'female']),
-    birthDate: z.iso.date(),
-    height: z.number(),
-    weight: z.number(),
-    activityLevel: z.number().min(1).max(5),
-    account: z.object({
-        name: z.string().min(1),
-        email: z.email(),
-        password: z.string().min(8, "Password deve ter pelo menos 8 caracteres"),
-    }),
+  goal: z.enum(['lose', 'maintain', 'gain']),
+  gender: z.enum(['male', 'female']),
+  birthDate: z.iso.date(),
+  height: z.number(),
+  weight: z.number(),
+  activityLevel: z.number().min(1).max(5),
+  account: z.object({
+    name: z.string().min(1),
+    email: z.email(),
+    password: z.string().min(8),
+  }),
 });
 
 export class SignUpController {
-    // Método handle recebe o HttpRequest e retorna um HttpResponse
-    static async handle({ body }: HttpRequest): Promise<HttpResponse> {
+  static async handle({ body }: HttpRequest): Promise<HttpResponse> {
+    const { success, error, data } = schema.safeParse(body);
 
-        // Valida o corpo da requisição usando o schema definido
-        // Se a validação falhar, retorna um erro 400 com os detalhes da validação
-        // Se a validação for bem-sucedida, prossegue com a lógica de criação
-        // do usuário e retorna um token de acesso fictício
-        // Você deve substituir isso pela lógica real de criação de usuário
-        // e geração de token de acesso
+    if (!success) {
+      return badRequest({ errors: error.issues });
+    }
 
-        const { success, error, data } = schema.safeParse(body);
+    const userAlreadyExists = await db.query.usersTable.findFirst({
+      columns: {
+        email: true,
+      },
+      where: eq(usersTable.email, data.account.email),
+    });
 
-        if (!success) {
-            return badRequest({
-                errors: error.issues
-            });
-        }
+    if (userAlreadyExists) {
+      return conflict({ error: 'This email is already in use.' });
+    }
 
-        // Aqui você pode adicionar a lógica para criar o usuário no banco de dados
-        // Por exemplo, chamar um serviço de usuário para criar o usuário com os dados validados
-        // E retornar o token de acesso ou qualquer outra informação necessária
-        // Neste exemplo, estamos apenas retornando um token de acesso fictício
-        // Você deve substituir isso pela lógica real de criação de usuário
-        // e geração de token de acesso
+    const { account, ...rest } = data;
+    const goals = calculateGoals({
+      activityLevel: rest.activityLevel,
+      birthDate: new Date(rest.birthDate),
+      gender: rest.gender,
+      goal: rest.goal,
+      height: rest.height,
+      weight: rest.weight,
+    });
+    
+    const hashedPassword = await hash(account.password, 8);
 
-        return created({
-            data,
-        });
-    } 
+
+    const [user] = await db
+      .insert(usersTable)
+      .values({
+        ...account,
+        ...rest,
+        ...goals,
+        password: hashedPassword,
+      })
+      .returning({
+        id: usersTable.id,
+      });
+
+    const accessToken = signAccessTokenFor(user.id);
+
+    return created({ accessToken });
+  }
 }
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,20 +1,36 @@
-import { HttpRequest, HttpResponse } from "../types/Http";
+import { HttpResponse } from '../types/Http';
 
 export function ok(body?: Record<string, any>): HttpResponse {
-    return {
-        statusCode: 200,
-        body: body ? body : undefined,
-    };
+  return {
+    statusCode: 200,
+    body,
+  };
 }
+
 export function created(body?: Record<string, any>): HttpResponse {
-    return {
-        statusCode: 201,
-        body: body ? body : undefined,
-    };
+  return {
+    statusCode: 201,
+    body,
+  };
 }
+
 export function badRequest(body?: Record<string, any>): HttpResponse {
-    return {
-        statusCode: 400,
-        body: body ? body : undefined,
-    };
+  return {
+    statusCode: 400,
+    body,
+  };
+}
+
+export function unauthorized(body?: Record<string, any>): HttpResponse {
+  return {
+    statusCode: 401,
+    body,
+  };
+}
+
+export function conflict(body?: Record<string, any>): HttpResponse {
+  return {
+    statusCode: 409,
+    body,
+  };
 }
